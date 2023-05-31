701df4e1efb03aab054a69bdbf6b5108eb529c6d
Borom OUM <borom.oum@chipmong.com>
2023-05-31 08:56:16 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container w-full h-screen">
+    <iframe style="position: absolute; 
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+
+  border: none;" src="https://map-overlay-cm-project.netlify.app/" scrolling="no"></iframe>
+  </div>
+</template>
